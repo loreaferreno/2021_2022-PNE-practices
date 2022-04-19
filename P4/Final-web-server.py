686338f5cc238cfe909c3ab3a148f03af3298be0
loreaferreno <l.ferreno.2021@alumnos.urjc.es>
2022-04-19 20:34:34 +0200

    practices redo

---
+++ P4/Final-web-server.py
@@ -0,0 +1,98 @@
+import socket
+import termcolor
+from pathlib import Path
+
+
+# -- Server network parameters
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def process_client(s):
+    # -- Receive the request message
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    # -- Split the request messages into lines
+    lines = req.splitlines()
+
+    # -- The request line is the first
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    lines1 = req_line.split(" ")
+    path = lines1[1]
+    method = lines1[0]
+    status = lines1[2]
+
+    # -- Generate the response message
+    # It has the following lines
+    # Status line
+    # header
+    # blank line
+    # Body (content to send)
+
+    # This new contents are written in HTML language
+    if path == "/":
+        body = Path("index.html").read_text()
+    elif path == "/info/A":
+        body = Path("A.html").read_text()
+    elif path == "/info/C":
+        body = Path("C.html").read_text()
+    elif path == "/info/T":
+        body = Path("T.html").read_text()
+    elif path == "/info/G":
+        body = Path("G.html").read_text()
+    else:
+        body = Path("error.html").read_text()
+
+    # -- Status line: We respond that everything is ok (200 code)
+    status_line = "HTTP/1.1 200 OK\n"
+
+    # -- Add the Content-Type header
+    header = "Content-Type: text/html\n"
+
+    # -- Add the Content-Length
+    header += f"Content-Length: {len(body)}\n"
+
+    # -- Build the message by joining together all the parts
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+# -------------- MAIN PROGRAM
+# ------ Configure the server
+# -- Listening socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Setup up the socket's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Become a listening socket
+ls.listen()
+
+print("SEQ Server configured!")
+
+# --- MAIN LOOP
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        ls.close()
+        exit()
+    else:
+
+        # Service the client
+        process_client(cs)
+
+        # -- Close the socket
+        cs.close()
---
+++ Session 12/exercise-1.py
@@ -0,0 +1,2 @@
+#The request line ins : Request line: GET /favicon.ico HTTP/1.1
+#favicon.ico
